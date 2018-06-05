873e6de4bb07f9075a5b577c838fc05fe16fc5a2
Alex Hall <alex@alexhall.org>
2018-06-05 13:06:14 -0700

    added train script

---
+++ train.sh
@@ -0,0 +1,1 @@
+python main.py --batch_size 8 --model FlowNet2 --optimizer=Adam --optimizer_lr=1e-4 --loss=MultiScale --loss_norm=L1 --loss_numScales=5 --loss_startScale=4 --optimizer_lr=1e-4 --crop_size 384 512 --training_dataset FlyingChairs --training_dataset_root /app/data/FlyingChairs/data --validation_dataset MpiSintelClean --validation_dataset_root /app/data/MPI-Sintel-complete/training
